4577b312795a98c71d6a1fdea3135fb1a5324f71
FacuBnz <facu.j.benitez@gmail.com>
2022-04-02 00:17:42 -0300

    enable jpaAuditing

--- src/main/java/com/disney/api/rest/disney/Application.java
+++ src/main/java/com/disney/api/rest/disney/Application.java
@@ -2,8 +2,12 @@ package com.disney.api.rest.disney;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import javax.persistence.EntityListeners;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Application {
 
 	public static void main(String[] args) {
