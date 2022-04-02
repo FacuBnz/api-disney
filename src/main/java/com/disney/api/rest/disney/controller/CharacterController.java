bb9941d329ad2a4dca8460e0bcb5e18e42d179b1
FacuBnz <facu.j.benitez@gmail.com>
2022-04-02 13:25:38 -0300

    Endpoint to obtain characters

--- src/main/java/com/disney/api/rest/disney/controller/CharacterController.java
+++ src/main/java/com/disney/api/rest/disney/controller/CharacterController.java
@@ -1,9 +1,33 @@
 package com.disney.api.rest.disney.controller;
 
+import com.disney.api.rest.disney.config.Response;
+import com.disney.api.rest.disney.service.CharacterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/characters")
 public class CharacterController {
+
+    @Autowired
+    private CharacterService characterService;
+
+    @GetMapping
+    public ResponseEntity<?> getAll(){
+        try{
+            return ResponseEntity.ok(characterService.getAll());
+        }catch (Exception e){
+            Response res = new Response();
+            res.setMessage(e.getMessage());
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(res);
+        }
+    }
+
+
 }
