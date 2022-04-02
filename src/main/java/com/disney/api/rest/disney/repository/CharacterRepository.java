93a402e46a089e9199eadee1cea64f42031e1d14
FacuBnz <facu.j.benitez@gmail.com>
2022-04-02 13:24:12 -0300

    Correction of the entity's import

--- src/main/java/com/disney/api/rest/disney/repository/CharacterRepository.java
+++ src/main/java/com/disney/api/rest/disney/repository/CharacterRepository.java
@@ -1,5 +1,6 @@
 package com.disney.api.rest.disney.repository;
 
+import com.disney.api.rest.disney.entity.Character;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CharacterRepository extends JpaRepository<Character, Integer> {
