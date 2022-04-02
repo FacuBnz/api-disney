a901f1d2d54ea774b84ad4f3f8d82d6324914270
FacuBnz <facu.j.benitez@gmail.com>
2022-04-02 02:23:59 -0300

    The user repository was created with 2 queries

---
+++ src/main/java/com/disney/api/rest/disney/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.disney.api.rest.disney.repository;
+
+import com.disney.api.rest.disney.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
