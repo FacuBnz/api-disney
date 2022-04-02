75b49f7163c7886fef4fbc9ddec589e5e8df79ac
FacuBnz <facu.j.benitez@gmail.com>
2022-04-02 16:52:49 -0300

    Enpoint to get the detail of a character

--- src/main/java/com/disney/api/rest/disney/controller/CharacterController.java
+++ src/main/java/com/disney/api/rest/disney/controller/CharacterController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,19 @@ public class CharacterController {
     public ResponseEntity<?> getAll(){
         try{
             return ResponseEntity.ok(characterService.getAll());
+        }catch (Exception e){
+            Response res = new Response();
+            res.setMessage(e.getMessage());
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(res);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getCharacterDetails(@PathVariable String id){
+        try{
+            return ResponseEntity.ok(characterService.findById(id));
         }catch (Exception e){
             Response res = new Response();
             res.setMessage(e.getMessage());
