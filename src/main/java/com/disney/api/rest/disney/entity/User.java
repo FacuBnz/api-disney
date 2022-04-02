38858221d4041a4902c51dac71a70e394dd2e0e2
FacuBnz <facu.j.benitez@gmail.com>
2022-04-02 00:16:38 -0300

    user entity creation

---
+++ src/main/java/com/disney/api/rest/disney/entity/User.java
@@ -0,0 +1,31 @@
+package com.disney.api.rest.disney.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime created_at;
+}
