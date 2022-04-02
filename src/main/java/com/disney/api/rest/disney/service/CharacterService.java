7ff1daa24b4b49701f588be0ec552ce2e4d5feeb
FacuBnz <facu.j.benitez@gmail.com>
2022-04-02 13:31:22 -0300

    method to call the converter to DTO

--- src/main/java/com/disney/api/rest/disney/service/CharacterService.java
+++ src/main/java/com/disney/api/rest/disney/service/CharacterService.java
@@ -1,7 +1,23 @@
 package com.disney.api.rest.disney.service;
 
+import com.disney.api.rest.disney.DTO.CharacterDTO;
+import com.disney.api.rest.disney.DTO.MappperDTO;
+import com.disney.api.rest.disney.entity.Character;
+import com.disney.api.rest.disney.repository.CharacterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class CharacterService {
+    @Autowired
+    private CharacterRepository characterRepository;
+
+    @Transactional(readOnly = true)
+    public List<CharacterDTO> getAll(){
+        List<Character> characters = characterRepository.findAll();
+        return MappperDTO.convertToCharacterDTO(characters);
+    }
 }
