4829bcffd65cf05de6aeee7ffaf31956b1df0bd0
FacuBnz <facu.j.benitez@gmail.com>
2022-04-05 21:54:45 -0300

    added notation repository

--- src/main/java/com/disney/api/rest/disney/repository/CharacterRepository.java
+++ src/main/java/com/disney/api/rest/disney/repository/CharacterRepository.java
@@ -3,9 +3,11 @@ package com.disney.api.rest.disney.repository;
 import com.disney.api.rest.disney.entity.Character;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CharacterRepository extends JpaRepository<Character, Integer> {
 
     List<Character> findCharacterByName(String name);
